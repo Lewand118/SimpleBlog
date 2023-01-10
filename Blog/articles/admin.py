2203359463747630bb9addb12cdc510efa2d2015
pawel.aleksander.mnich <pawel.aleksander.mnich@gmail.com>
2023-01-10 22:42:06 +0100

    feat: adding article categories

--- Blog/articles/admin.py
+++ Blog/articles/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 
-# Register your models here.
 
-# admin.site.register(Article)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'published', 'author')
